542dd38d02ebfc2c4591026ebdf367b9f664fccf
MAREK <marek.armulik@gmail.com>
2024-04-10 10:24:59 +0300

    master updated settings

---
+++ src/components/YearDropdown.vue
@@ -0,0 +1,18 @@
+
+
+<template>
+  <select class="form-select" aria-label="Default select example">
+    <option selected>Vali aasta</option>
+    <option value="1">1993</option>
+    <option value="2">1983</option>
+    <option value="3">1996</option>
+  </select>
+</template>
+
+
+<script>
+export default {
+  name: "YearDropdown"
+}
+</script>
+
--- src/views/RepairCalculatorView.vue
+++ src/views/RepairCalculatorView.vue
@@ -2,7 +2,9 @@
   <div class="container text-center">
     <div class="row justify-content-center">
       <div class="col">
-        <BrandDropdown/>
+        <BrandDropdown class="mt-6"/>
+        <ModelDropdown class="mt-3"/>
+        <YearDropdown class="mt-3"/>
 
       </div>
       <div class="col">
@@ -25,9 +27,13 @@
 
 <script>
 import BrandDropdown from "@/components/BrandDropdown.vue";
+import ModelDropdown from "@/components/ModelDropdown.vue";
+import YearDropdown from "@/components/YearDropdown.vue";
+
+
 
 export default {
-  components: { BrandDropdown},
+  components: { BrandDropdown, YearDropdown, ModelDropdown},
   name: 'RepairCalculatorView'
 
 }
