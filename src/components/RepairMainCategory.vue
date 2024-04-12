bb1368b2797903ced87cf7142b1d3bcbe2a90375
MAREK <marek.armulik@gmail.com>
2024-04-12 10:33:35 +0300

    master mainCategorySelect

--- src/components/RepairMainCategory.vue
+++ src/components/RepairMainCategory.vue
@@ -3,16 +3,7 @@
   <div class="row">
     <div class="col">
       <div class="list-group" id="list-tab" role="tablist">
-        <a class="list-group-item list-group-item-action active" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">Home</a>
-        <a class="list-group-item list-group-item-action" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">Profile</a>
-        <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
-        <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
-        <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
-        <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
-        <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
-        <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
-        <a class="list-group-item list-group-item-action" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">Messages</a>
-        <a class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">Settings</a>
+        <a v-for="repairMainCategory in repairMainCategories" class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">{{repairMainCategory.repairMainCategoryName}}</a>
       </div>
     </div>
 
@@ -25,11 +16,29 @@ export default {
   name: "RepairMainCategory",
   data(){
     return{
+      selectedRepairMainCategoryId:0,
+      repairMainCategories:[
+        {
+          repairMainCategoryId:0,
+          repairMainCategoryName:''
+        }
+      ]
 
     }
   },
   methods:{
-
+    sendGetMainCategoryRequest: function () {
+      this.$http.get("/maincategory")
+          .then(response => {
+            this.repairMainCategories = response.data
+          })
+          .catch(error => {
+            // const errorResponseBody = error.response.data
+          })
+    },
+  },
+  beforeMount() {
+    this.sendGetMainCategoryRequest()
   }
 }
 </script>
