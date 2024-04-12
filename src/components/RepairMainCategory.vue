c055d7d5ee3b0d0a68ee6eef053c6a8883171674
MAREK <marek.armulik@gmail.com>
2024-04-12 11:02:27 +0300

    master mainCategorySelect

--- src/components/RepairMainCategory.vue
+++ src/components/RepairMainCategory.vue
@@ -2,10 +2,12 @@
 <template>
   <div class="row">
     <div class="col">
-      <div class="list-group" id="list-tab" role="tablist">
-        <a v-for="repairMainCategory in repairMainCategories" class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">{{repairMainCategory.repairMainCategoryName}}</a>
+      <div  @change="emitSelectedRepairMainCategoryId" class="list-group" id="list-tab" role="tablist">
+        <a v-model="selectedRepairMainCategoryId" v-for="repairMainCategory in repairMainCategories" class="list-group-item list-group-item-action" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">{{repairMainCategory.repairMainCategoryName}}</a>
       </div>
     </div>
+
+    {{selectedRepairMainCategoryId}}
 
   </div>
 </template>
@@ -36,6 +38,9 @@ export default {
             // const errorResponseBody = error.response.data
           })
     },
+    emitSelectedRepairMainCategoryId(){
+      this.$emit(`event-category-change`, this.selectedRepairMainCategoryId)
+    }
   },
   beforeMount() {
     this.sendGetMainCategoryRequest()
