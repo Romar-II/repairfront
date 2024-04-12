0bb0fb311d68d954a1c940a6e461ba14ae085ab2
Stiven <stiven161@gmail.com>
2024-04-12 11:39:02 +0300

    F WIP

--- src/components/RepairMainCategory.vue
+++ src/components/RepairMainCategory.vue
@@ -47,6 +47,7 @@ export default {
     },
     handleItemClick(repairMainCategory){
       this.selectedRepairMainCategoryId = repairMainCategory
+      this.emitSelectedRepairMainCategoryId()
     }
   },
   beforeMount() {
