783f6ca3d8c336cfbf9de53248ba2d161759516c
Alar <alar.urke@gmail.com>
2024-04-14 17:32:05 +0300

    F1, BD, MD, RCV updata

--- src/components/YearDropdown.vue
+++ src/components/YearDropdown.vue
@@ -2,17 +2,40 @@
 
 <template>
   <select class="form-select" aria-label="Default select example">
-    <option selected>Vali aasta</option>
-    <option value="1">1993</option>
-    <option value="2">1983</option>
-    <option value="3">1996</option>
+    <option selected disabled hidden>Vali aasta</option>
+    <option v-for="year in years" :value="year.yearId" :key="year.yearId" >{{year.yearNumber}}</option>
+
   </select>
 </template>
 
 
 <script>
 export default {
-  name: "YearDropdown"
+  name: "YearDropdown",
+  data() {
+    return {
+      yearId: 0,
+      years: [
+        {
+          yearId: 0,
+          yearNumber: ''
+        }
+      ]
+    }
+  },
+  methods: {
+
+    sendGetModelRequest() {
+      this.$http.get(`/years/${this.brandId}`)//kas siin ka brandID või ei ole sellel sõltuvust?
+          .then(response => {
+            this.years = response.data
+          })
+          .catch(error => {
+            // const errorResponseJSON = error.response.data
+          })
+    }
+
+  }
 }
 </script>
 
