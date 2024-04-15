082471796beb1c8212f12b66263d0a68b29609e2
MAREK <marek.armulik@gmail.com>
2024-04-15 11:49:30 +0300

    master mainCategorySelect

---
+++ src/components/OnlineShopAccordion.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "OnlineShopAccordion"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/OnlineShopView.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "OnlineShopView"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
