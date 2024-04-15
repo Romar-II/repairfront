673ecc328925979f3b458c21a781f9ef84944271
MAREK <marek.armulik@gmail.com>
2024-04-15 12:06:35 +0300

    master mainCategorySelect

--- src/views/OnlineShopView.vue
+++ src/views/OnlineShopView.vue
@@ -1,13 +1,23 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-5 mt-5">
+        <div class="row">
+          <div class="col-4">
+
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        grid toodetest
+      </div>
+
+    </div>
+  </div>
+</template>
+
 <script>
 export default {
   name: "OnlineShopView"
 }
 </script>
-
-<template>
-
-</template>
-
-<style scoped>
-
-</style>
