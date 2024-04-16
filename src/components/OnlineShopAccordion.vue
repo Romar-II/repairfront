bc4fd98abdbb902c91e0eaeac5cf32cf4a0dfd17
MAREK <marek.armulik@gmail.com>
2024-04-16 15:34:03 +0300

    master mainCategorySelect

--- src/components/OnlineShopAccordion.vue
+++ src/components/OnlineShopAccordion.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="col-4">
-    <div v-for="(mainCategory, index) in mainCategories" :key="index" class="list-group" >
-      <button type="button" class="list-group-item" @click="toggleCollapse(index)" @change="selectSearchCriteria(mainCategory.categoryId)" data-bs-toggle="button" aria-pressed="false" autocomplete="off">
+
+    <div v-for="mainCategory in mainCategories" :key="mainCategory.categoryId" class="list-group" >
+      <button type="button" class="list-group-item" @click="handleMainCategoryClick(mainCategory.categoryId)" data-bs-toggle="button" aria-pressed="false" autocomplete="off">
         {{ mainCategory.categoryName }}
       </button>
       <div v-if="mainCategory.isCollapsed">
-        <button v-for="(subCategory, subIndex) in mainCategory.subCategories" :key="subIndex" type="button" class="list-group-item" data-bs-toggle="button" aria-pressed="false" autocomplete="off">
+        <button v-for="subCategory in mainCategory.subCategories" @change="selectSearchCriteria(subCategory.subCategoryId)" :key="subCategory.subCategoryId" type="button" class="list-group-item" data-bs-toggle="button" aria-pressed="false" autocomplete="off">
           {{ subCategory.subCategoryName }}
         </button>
       </div>
     </div>
+    {{mainCategories}}
+    {{searchCriteria.mainCategoryId}}
+    {{searchCriteria.subCategoryId}}
+    {{mainCategories.categoryId}}
+    {{testmuutuja}}
   </div>
 </template>
 
@@ -17,6 +23,7 @@
 export default {
   data() {
     return {
+      testmuutuja:0,
       mainCategories: [
         {
           categoryId: 0,
@@ -32,15 +39,19 @@ export default {
       ],
       searchCriteria: [
         {
-          mainCategoryId:0,
-          subCategoryId:0
+          mainCategoryId:1,
+          subCategoryId:1
         }
       ]
     };
   },
   methods: {
+    handleMainCategoryClick(categoryId){
+      this.toggleCollapse(categoryId)
+      this.selectSearchCriteria(categoryId)
+    },
     toggleCollapse(index) {
-      this.mainCategories[index].isCollapsed = !this.mainCategories[index].isCollapsed;
+      this.mainCategories[index-1].isCollapsed = !this.mainCategories[index-1].isCollapsed;
     },
     sendCategoriesRequest() {
       this.$http.get("/shop/categories")
@@ -53,16 +64,29 @@ export default {
           })
     },
     updateMainCategories() {
-      this.mainCategories = this.mainCategories.map(function (category) {
+      this.mainCategories = this.mainCategories.map((category) => {
         return {
           ...category,
           isCollapsed: false
         }
       })
     },
-    selectSearchCriteria(){
+    selectSearchCriteria(categoryId, subCategoryId){
+      this.searchCriteria.mainCategoryId = categoryId
+      this.searchCriteria.subCategories=subCategoryId
+      this.sendTestStuff(categoryId)
+      this.sendTestStuff(subCategoryId)
 
-    }
+    },
+    sendTestStuff(categoryId) {
+      this.$http.get(`/some/${categoryId}`)
+          .then(response => {
+            const responseBody = response.data
+          })
+          .catch(error => {
+            // const errorResponseBody = error.response.data
+          })
+    },
 
   },
   beforeMount: function () {
