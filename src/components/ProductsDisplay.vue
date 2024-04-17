dbeb5ec8183b7c2254ae47c49ff922caae9fac15
MAREK <marek.armulik@gmail.com>
2024-04-17 08:28:06 +0300

    master mainCategorySelect

---
+++ src/components/ProductsDisplay.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div class="d-flex justify-content-end me-3 mt-3
+">
+    <button class="btn btn-secondary btn-sm dropdown-toggle " type="button" data-bs-toggle="dropdown" aria-expanded="false">
+      Mitu tk korraga
+    </button>
+    <ul class="dropdown-menu">
+      ...
+    </ul>
+  </div>
+  <div class="container text-center">
+  <div class="row">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      columd
+    </div>
+  </div>
+
+
+
+</template>
+
+<script>
+export default {
+  name: "ProductsDisplay"
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
