65888e0792e1797e8e40831141ffed3f7802563d
MAREK <marek.armulik@gmail.com>
2024-04-17 14:11:36 +0300

    master mainCategorySelect

--- src/components/OnlineShopAccordion.vue
+++ src/components/OnlineShopAccordion.vue
@@ -5,14 +5,14 @@
     <div class="col">
       <div class="list-group" id="list-tab" role="tablist">
         <div v-for="mainCategory in mainCategories">
-          <button class="list-group-item list-group-item-action" :key="mainCategory.categoryId"
+          <button  class="list-group-item list-group-item-action" ata-bs-toggle="button"  :key="mainCategory.categoryId"
                   @click="handleMainCategory(mainCategory.categoryId)" :id="mainCategory.categoryId">
             {{ mainCategory.categoryName }}
           </button>
           <div v-if="!mainCategory.isCollapsed">
             <button v-for="subCategory  in mainCategory.subCategories"
                     @click="handleSubCategory(mainCategory.categoryId,subCategory.subCategoryId)"
-                    :key="subCategory.subCategoryId" type="button" class="list-group-item list-group-item-action">
+                    :key="subCategory.subCategoryId" type="button" class="list-group-item list-group-item-action" >
               {{ subCategory.subCategoryName }}
             </button>
           </div>
@@ -24,41 +24,28 @@
       </div>
 
 
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
+<!--      <div class="list-group" id="list-tab" role="tablist">-->
+<!--        <button v-for="(mainCategory1, index) in mainCategories" class="list-group-item list-group-item-action"-->
+<!--                :key="mainCategory1.categoryId"-->
+<!--                @click="handleMainCategoryClick(mainCategory1.categoryId, index)" :id="mainCategory1.categoryId"-->
+<!--                data-bs-toggle="list"-->
+<!--                href="#list-settings" role="tab" aria-controls="list-settings">{{ mainCategory1.categoryName }}-->
+<!--          <div v-if="mainCategory1.isCollapsed">-->
+<!--            <button v-for="(subCategory, index) in mainCategory1.subCategories"-->
+<!--                    @click="handleSubCategoryClick(subCategory.subCategoryId, index)"-->
+<!--                    :key="subCategory.subCategoryId" type="button" class="list-group-item list-group-item-action"-->
+<!--                    data-bs-toggle="list"-->
+<!--                    aria-pressed="false" autocomplete="off">-->
+<!--              {{ subCategory.subCategoryName }}-->
+<!--            </button>-->
+<!--          </div>-->
+<!--        </button>-->
 
+<!--      </div>-->
 
-      <div class="list-group" id="list-tab" role="tablist">
-        <button v-for="(mainCategory1, index) in mainCategories" class="list-group-item list-group-item-action"
-                :key="mainCategory1.categoryId"
-                @click="handleMainCategoryClick(mainCategory1.categoryId, index)" :id="mainCategory1.categoryId"
-                data-bs-toggle="list"
-                href="#list-settings" role="tab" aria-controls="list-settings">{{ mainCategory1.categoryName }}
-          <div v-if="mainCategory1.isCollapsed">
-            <button v-for="(subCategory, index) in mainCategory1.subCategories"
-                    @click="handleSubCategoryClick(subCategory.subCategoryId, index)"
-                    :key="subCategory.subCategoryId" type="button" class="list-group-item list-group-item-action"
-                    data-bs-toggle="list"
-                    aria-pressed="false" autocomplete="off">
-              {{ subCategory.subCategoryName }}
-            </button>
-          </div>
-        </button>
+      {{ selectedCategoryId }}
+      {{ selectedSubCategoryId}}
 
-      </div>
-
-      {{ selectedSubCategoryId }}
 
     </div>
   </div>
@@ -90,18 +77,23 @@ export default {
           productImageData: '',
         }
       ],
-      selectedCategoryId: 0,
-      selectedSubCategoryId: 0,
+      selectedCategoryId:0,
+      selectedSubCategoryId:0
     };
   },
   methods: {
     handleMainCategory(categoryId) {
       this.toggleMainCategory(categoryId)
-      // saada sõnum
-      // categoryId = categoryId
-      // subCategory = 0
-    },
+      this.selectedCategoryId=categoryId
+      this.selectedSubCategoryId=0
+      this.sendProductRequest()
 
+    },
+    handleSubCategory(categoryId, subCategoryId) {
+      this.selectedCategoryId=categoryId
+      this.selectedSubCategoryId =subCategoryId
+      this.sendProductRequest()
+    },
     toggleMainCategory(categoryId) {
       this.mainCategories.forEach((category) => {
         if (category.categoryId === categoryId) {
@@ -112,11 +104,7 @@ export default {
       });
     },
 
-    handleSubCategory(categoryId, subCategoryId) {
-      // saada sõnum
-      // categoryId = categoryId
-      // subCategory = subCategoryId
-    },
+
 
 
     handleSubCategoryClick(subCategoryId) {
@@ -142,7 +130,7 @@ export default {
             }
           }
       ).then(response => {
-        const responseBody = response.data
+        this.products = response.data
       }).catch(error => {
         const errorResponseBody = error.response.data
       })
