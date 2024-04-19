e26a61f942f5e1e782bb29f2f6d74b5b3a5ae424
Alar <alar.urke@gmail.com>
2024-04-19 13:50:01 +0300

    F2-YEAR updated RepairMainCategory.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,19 +2,19 @@
   <div class="text-center">
 
     <div class=" col">
-      <h1>Remondileht</h1>
+      <h1>Autoremondi pakkumine paari klikiga!</h1>
     </div>
 
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="2000">
-          <img src="../assets/remont.png" class="d-block w-70" alt="...">
+          <img src="../assets/pilt1.png" class="d-block w-50" alt="...">
         </div>
         <div class="carousel-item" data-bs-interval="2000">
-          <img src="../assets/parandus1.png" class="d-block w-100" alt="...">
+          <img src="../assets/pilt2.png" class="d-block w-50" alt="...">
         </div>
-        <div class="carousel-item">
-          <img src="../assets/logo.png" class="d-block w-100" alt="...">
+        <div class="carousel-item" data-bs-interval="2000">
+          <img src="../assets/pilt3.png" class="d-block w-50" alt="...">
         </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
