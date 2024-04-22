3eecf32b4e3cc446bbd08a52f53fdf0bc0be74e0
Alar <alar.urke@gmail.com>
2024-04-22 11:54:55 +0300

    F2-YEAR updated RepairMainCategory.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,30 +5,27 @@
       <h1>Autoremondi pakkumine paari klikiga!</h1>
     </div>
 
-    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-      <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="2000">
-          <img src="../assets/pilt1.png" class="d-block w-50" alt="...">
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-          <img src="../assets/pilt2.png" class="d-block w-50" alt="...">
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-          <img src="../assets/pilt3.png" class="d-block w-50" alt="...">
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-          <img src="../assets/vaade1.jpg" class="d-block w-50" alt="...">
-        </div>
+
+
+          <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" style="width: 30%;">
+            <div class="carousel-inner">
+              <div class="carousel-item active" data-bs-interval="2000">
+                <img src="../assets/pilt1.png" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item" data-bs-interval="6000">
+                <img src="../assets/pilt2.png" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item" data-bs-interval="2000">
+                <img src="../assets/pilt3.png" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item" data-bs-interval="2000">
+                <img src="../assets/vaade1.jpg" class="d-block w-100" alt="...">
+              </div>
+
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
     </div>
+
+
   </div>
 
 </template>
@@ -44,8 +41,8 @@ export default {
 
 <style>
 
-.text-white{
-  color:white;
+.text-white {
+  color: white;
 }
 
 </style>
