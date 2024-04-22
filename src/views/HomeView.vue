8fccc75ea15476324216d65f923f976b3f6252ad
Alar <alar.urke@gmail.com>
2024-04-22 15:27:00 +0300

    F2-YEAR updated RepairMainCategory.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,27 +5,28 @@
       <h1>Autoremondi pakkumine paari klikiga!</h1>
     </div>
 
-
-
-          <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" style="width: 30%;">
-            <div class="carousel-inner">
-              <div class="carousel-item active" data-bs-interval="2000">
-                <img src="../assets/pilt1.png" class="d-block w-100" alt="...">
-              </div>
-              <div class="carousel-item" data-bs-interval="6000">
-                <img src="../assets/pilt2.png" class="d-block w-100" alt="...">
-              </div>
-              <div class="carousel-item" data-bs-interval="2000">
-                <img src="../assets/pilt3.png" class="d-block w-100" alt="...">
-              </div>
-              <div class="carousel-item" data-bs-interval="2000">
-                <img src="../assets/vaade1.jpg" class="d-block w-100" alt="...">
-              </div>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" style="width: 30%;">
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="2000">
+          <img src="../assets/pilt1.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+          <img src="../assets/pilt2.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+          <img src="../assets/pilt3.png" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+          <img src="../assets/vaade1.jpg" class="d-block w-100" alt="...">
+        </div>
 
       </div>
     </div>
-
-
+    <div class="position-relative">
+      <div class="position-absolute top-0 end-0">
+        <img src="../assets/garantii2.png" width="200" height="150">
+      </div>
+    </div>
   </div>
 
 </template>
