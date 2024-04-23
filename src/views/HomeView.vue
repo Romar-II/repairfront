d56492787551721700f315a7b5d3c737a5343fe4
Alar <alar.urke@gmail.com>
2024-04-23 12:00:20 +0300

    F2-YEAR updated RepairMainCategory.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,32 @@
 <template>
   <div class="text-center">
-
-    <div class=" col text-white ">
-      <h1>Autoremondi pakkumine paari klikiga!</h1>
-    </div>
-
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" style="width: 30%;">
-      <div class="carousel-inner">
-        <div class="carousel-item active" data-bs-interval="2000">
-          <img src="../assets/pilt1.png" class="d-block w-100" alt="...">
+    <div class="row">
+      <div class=" col text-white ">
+        <h1>Autoremondi pakkumine paari klikiga!</h1>
+      </div>
+      <div class="row">
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" style="width: 30%;">
+          <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="2000">
+              <img src="../assets/pilt1.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+              <img src="../assets/pilt2.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+              <img src="../assets/pilt3.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item" data-bs-interval="2000">
+              <img src="../assets/vaade1.jpg" class="d-block w-100" alt="...">
+            </div>
+          </div>
         </div>
-        <div class="carousel-item" data-bs-interval="2000">
-          <img src="../assets/pilt2.png" class="d-block w-100" alt="...">
+        <div class="col text-end">
+          <img src="../assets/garantii2.png" width="200" height="150">
         </div>
-        <div class="carousel-item" data-bs-interval="2000">
-          <img src="../assets/pilt3.png" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item" data-bs-interval="2000">
-          <img src="../assets/vaade1.jpg" class="d-block w-100" alt="...">
-        </div>
-
       </div>
     </div>
-    <div class="col text-end">
-      <img src="../assets/garantii2.png" width="200" height="150">
-    </div>
+
   </div>
 
 </template>
