4f7b389a64b92fea28336b061b88d848b7261e81
Alar <alar.urke@gmail.com>
2024-04-24 13:32:24 +0300

    F2-YEAR updated RepairMainCategory.vue

--- src/App.vue
+++ src/App.vue
@@ -64,50 +64,5 @@ export default {
 </script>
 
 <style>
-#app {
-  min-height: 100vh; /* Minimum height of 100% of the viewport height */
-  //background-image: url('../src/assets/RepairBoyzBackUpdated.jpg'); /* Background Image */
-  background-size: cover; /* Cover the entire container */
-  background-repeat: no-repeat; /* No repeat */
-  background-attachment: fixed;
-}
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #151f26;
-
-
-
-  /* Set background size to cover the entire container */
-  background-size: cover;
-  /* Center the background image */
-  background-position: center;
-  /* Make sure the background image is fixed so it doesn't scroll with content */
-  background-attachment: fixed;
-  /* Add some fallback background color */
-  background-color: #4cb2f3; /* Fallback color */
-  /* Ensure content takes up the full height of the viewport */
-  min-height: 100vh;
-  /* Other styles for your app container */
-}
-
-.text-white{
-  color:white;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2b2d30;
-}
-
-nav a.router-link-exact-active {
-  color: #46c1cc;
-}
 </style>
