8cb38a739d3f7602acbdb431a27c3c9ac273c652
Stiven <stiven161@gmail.com>
2024-04-24 15:25:06 +0300

    login

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,8 +1,31 @@
 
 <template>
-  <div class="container">
+  <div class="container justify-content-center">
     <h1>Registration Form</h1>
-    <!-- Your registration form here -->
+    <div class="mt-4 container text-start col-3">
+      <label for="username" class="form-label">Kasutajanimi</label>
+      <input v-model="username" type="username" class="form-control border border-success " id="username">
+
+      <label for="password" class="form-label">Parool</label>
+      <input v-model="password" type="password" class="form-control border border-success " id="password">
+
+      <label for="password" class="form-label">Parool uuesti</label>
+      <input v-model="password" type="password" class="form-control border border-success " id="password">
+
+      <label for="password" class="form-label">E-Mail</label>
+      <input v-model="password" type="password" class="form-control border border-success " id="password">
+
+      <label for="password" class="form-label">Automark</label>
+      <input v-model="password" type="password" class="form-control border border-success " id="password">
+
+      <label for="password" class="form-label">Mudel</label>
+      <input v-model="password" type="password" class="form-control border border-success " id="password">
+    </div>
+    <div>
+      <button>Registreeri 50eur eest</button>
+    </div>
+
+
   </div>
  </template>
 
