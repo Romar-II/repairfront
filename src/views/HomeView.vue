d46d59e4cb1c0b6f683f35e786dc879d4ed73361
Alar <alar.urke@gmail.com>
2024-04-25 09:44:40 +0300

    F2-YEAR updated RepairMainCategory.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,23 +6,23 @@
       </div>
       <div class="row">
         <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" style="width: 30%;">
-          <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="2000">
+          <div class="carousel-i">
+            <div class="carousel-item active" data-bs-interval="4000">
               <img src="../assets/pilt1.png" class="d-block w-100" alt="...">
             </div>
-            <div class="carousel-item" data-bs-interval="2000">
+            <div class="carousel-item" data-bs-interval="4000">
               <img src="../assets/pilt2.png" class="d-block w-100" alt="...">
             </div>
-            <div class="carousel-item" data-bs-interval="2000">
+            <div class="carousel-item" data-bs-interval="4000">
               <img src="../assets/pilt3.png" class="d-block w-100" alt="...">
             </div>
-            <div class="carousel-item" data-bs-interval="2000">
+            <div class="carousel-item" data-bs-interval="4000">
               <img src="../assets/vaade1.jpg" class="d-block w-100" alt="...">
             </div>
           </div>
         </div>
-        <div class="col text-end-6">
-          <img src="../assets/garantii2.png" class="rounded-pill" style="width: 30%;">
+        <div class="col text-end-5">
+          <img src="../assets/garantii2.png" class="rounded-pill" style="width:30%;">
         </div>
       </div>
 
